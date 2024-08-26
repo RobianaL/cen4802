f964aba6c5d1d7ec30144e87a7c3b4369df7c92e
RobianaL <rlabady@mail.valenciacollege.edu>
2024-08-25 21:04:51 -0400

    CEN4802CAssignment1.java

--- CEN4802CAssignment1.java
+++ CEN4802CAssignment1.java
@@ -10,20 +10,19 @@ package com.mycompany.cen4802cassignment1;
  */
 public class CEN4802CAssignment1 {
 
-    //Recursive method to find the nth number
     public static int fibonacci(int n) {
         if (n <= 1) {
             return n;
-        }//end if
+        }
         else {
             return fibonacci(n - 1) + fibonacci(n - 2);
-        }//end else
+        }
     }
 
     public static void main(String[] args) {
-        int n = 10; // Get the 10th Fibonacci number
+        int n = 10;
         int result = fibonacci(n);
         System.out.println("The " + n + "th term of the Fibonacci sequence is " + result + ".");
-    }//end main
+    }
     
-}//end CEN4802CAssignment1
+}
